1ae7a5ed158b6954670d4ee20d10783a645f002d
Divya <divyatadooru78gmail.com>
2020-03-20 23:21:58 +0530

    Adding files

---
+++ Level/ReadWrite/ReadWrite/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+       
+        Console.WriteLine("Please enter your First Name");
+        string FName = Console.ReadLine();
+        Console.WriteLine("Please enter your Last Name");
+        string LName = Console.ReadLine();
+        Console.WriteLine("Hello {0} {1}",FName,LName);
+    }
+}
---
+++ Level/StringType/StringType/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+    class Program
+    {
+        public static void Main()
+        {
+           
+            string Name = "\"Divya\"";
+            Console.WriteLine(Name);
+
+           
+            Name = "One\nTwo\nThree";
+            Console.WriteLine(Name);
+
+            // Displaying new line character in c#
+            Name = "c:\\Tadooru\\Divya\\Balkrishna\\Renuka";
+            Console.WriteLine(Name);
+
+            // C# verbatim literal
+            Name = @"c:\Tadooru\Divya\Balkrishna\Renuka";
+            Console.WriteLine(Name);
+        }
+    }
+
+
