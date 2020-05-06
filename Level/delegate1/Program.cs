e8c3242497f2546e74468e4016a7e899655903cc
Divya2578 <61861837+Divya2578@users.noreply.github.com>
2020-05-06 20:42:43 +0530

    C#

---
+++ Level/delegate1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public delegate void HelloDelegate(string Message);
+class Program
+{
+    public static void Main()
+    {
+        HelloDelegate del = new HelloDelegate(Hello);
+        del("Hello from Delegates");
+    }
+
+    public static void Hello(string strMessage)
+    {
+        Console.WriteLine(strMessage);
+    }
+}
