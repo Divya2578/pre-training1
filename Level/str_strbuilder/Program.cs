6115544207fafeca55c885c36197aa9bff7ed404
Divya2578 <61861837+Divya2578@users.noreply.github.com>
2020-05-06 20:49:29 +0530

    c#

---
+++ Level/str_strbuilder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Example of String");
+        string userString = "C#";
+        userString += " Video";
+        userString += " Tutorial";
+        userString += " for";
+        userString += " beginners";
+        Console.WriteLine(userString);
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Example of StringBuilder");
+        StringBuilder userStringBuilder = new StringBuilder("C#");
+        userStringBuilder.Append(" Video");
+        userStringBuilder.Append(" Tutorial");
+        userStringBuilder.Append(" for");
+        userStringBuilder.Append(" beginners");
+        Console.WriteLine(userStringBuilder.ToString());
+    }
+}
