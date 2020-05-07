58b6cc4e47c6fbeeefb1b37c69e47ecd607020da
Divya2578 <61861837+Divya2578@users.noreply.github.com>
2020-05-07 17:06:37 +0530

    Add files via upload

---
+++ Level/optionparas1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    public static void AddNumbers(int firstNumber, int secondNumber,
+       int[] restOfNumbers)
+    {
+        int result = firstNumber + secondNumber;
+        if (restOfNumbers != null)
+        {
+            foreach (int i in restOfNumbers)
+            {
+                result += i;
+            }
+        }
+
+        Console.WriteLine("Sum = " + result);
+    }
+    public static void Main()
+    {
+        AddNumbers(10, 20, new int[] { 30, 40 });
+        Test(1, c:2);
+    }
+    public static void Test(int a, int b = 10, int c = 20)
+    {
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+        Console.WriteLine("c = " + c);
+    }
+  
+
+
+}
