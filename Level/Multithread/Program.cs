d431694215dc5f5394515a8cea398820e02ed0e3
Divya2578 <61861837+Divya2578@users.noreply.github.com>
2020-05-09 21:14:22 +0530

    c#

---
+++ Level/Multithread/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+namespace ThreadStartDelegateExample
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Thread T1 = new Thread(Number.PrintNumbers);
+            T1.Start();
+        }
+    }
+
+    class Number
+    {
+        public static void PrintNumbers()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
---
+++ Level/Thread1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+namespace ThreadingExample
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter the target number");
+            
+            int target = Convert.ToInt32(Console.ReadLine());
+
+   
+            Number number = new Number(target);
+    
+            Thread T1 = new Thread(new ThreadStart(number.PrintNumbers));
+          
+            T1.Start();
+        }
+    }
+
+ 
+    class Number
+    {
+        int _target;
+        public Number(int target)
+        {
+            this._target = target;
+        }
+        public void PrintNumbers()
+        {
+            for (int i = 1; i <= _target; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
